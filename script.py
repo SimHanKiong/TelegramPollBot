2757d0e7d680de312f19d8cb3a92c84f5639dacb
Han Kiong <61400768+SimHanKiong@users.noreply.github.com>
2023-08-11 18:09:38 +0800

    Create script.py

---
+++ script.py
@@ -0,0 +1,13 @@
+import json
+import request
+
+base_url = 'https://api.telegram.org/bot6058334295:AAGGjs89mcyHLjIrFzxRZKhZ4twQKowLx8U/sendPoll'
+
+parameters = {
+  'chat_id': '-920155043',
+  'question': 'Dinz',
+  'options': json.dumps(['6.30 pm', '7 pm', '7.30 pm'])
+  'is_anonymous': False
+}
+
+request.get(base_url, data = parameters)
