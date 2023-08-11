3b8ab123b21e98b58e82bf0ac600b984e7b91ced
Han Kiong <61400768+SimHanKiong@users.noreply.github.com>
2023-08-11 19:41:42 +0800

    Update script.py

--- script.py
+++ script.py
@@ -7,7 +7,8 @@ parameters = {
   'chat_id': '-920155043',
   'question': 'Dinz',
   'options': json.dumps(['6.30 pm', '7 pm', '7.30 pm']),
-  'is_anonymous': False
+  'is_anonymous': False,
+  'allows_multiple_answers': True
 }
 
 requests.get(base_url, data = parameters)
